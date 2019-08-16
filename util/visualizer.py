1d6521231f6d7f240047d345b0c18f5aecab9724
times2049 <times21c@gmail.com>
2019-08-15 19:33:57 -0800

    add boxplot and difference map.

--- util/visualizer.py
+++ util/visualizer.py
@@ -8,6 +8,71 @@ import scipy.misc
 from io import BytesIO         # Python 3.x
 
 
+def difference_map(pred, gt, save_fold, img_name, method_name, min_value=0, max_value=500):
+    save_fold = os.path.join(save_fold, method_name)
+    if not os.path.isdir(save_fold):
+        os.makedirs(save_fold)
+
+    raw_img_fold = os.path.join(save_fold, 'raw')
+    if not os.path.isdir(raw_img_fold):
+        os.makedirs(raw_img_fold)
+
+    pred = (pred[:, :, 1]).astype(np.float32)
+    gt = (gt[:, :, 1]).astype(np.float32)
+
+    diff_map = np.abs(pred - gt)
+    diff_map[0, 0] = min_value
+    diff_map[-1, -1] = max_value
+
+    plt.imshow(diff_map, vmin=min_value, vmax=max_value, cmap='afmhot')
+    cb = plt.colorbar(ticks=np.linspace(min_value, max_value, num=3))
+    cb.ax.set_yticklabels(cb.ax.get_yticklabels(), fontsize=12)
+    plt.axis('off')
+    # plt.show()
+
+    plt.savefig(os.path.join(save_fold, img_name), bbox_inches='tight')
+    plt.close()
+
+    # save raw pred_img
+    cv2.imwrite(os.path.join(raw_img_fold, img_name), pred)
+    
+    
+def draw_box_plot(data_list, method_names):
+    filenames = ['MAE', 'RMSE', 'PSNR', 'SSIM', 'PCC']
+    expressions = [' (lower is better)', ' (lower is better)', ' (higher is better)', ' (higher is better)',
+                   '(higher is better)']
+    colors = ['red', 'green', 'blue', 'aquamarine', 'aqua']  # purple
+
+    for idx, data in enumerate(data_list):
+        fig1, ax1 = plt.subplots(figsize=(2.5*len(method_names), 6))
+        box = ax1.boxplot(np.transpose(data), patch_artist=True, showmeans=True, sym='r+', vert=True)
+
+        # connect mean values
+        y = data.mean(axis=1)
+        ax1.plot(range(1, len(method_names)+1), y, 'r--')
+
+        for patch, color in zip(box['boxes'], colors):
+            patch.set(facecolor=color, alpha=0.5, linewidth=1)
+
+        # scatter draw datapoints
+        x_vals, y_vals = [], []
+        for i in range(data_list[0].shape[0]):
+            # move x coordinate to not overlapping
+            x_vals.append(np.random.normal(i + 0.7, 0.04, data.shape[1]))
+            y_vals.append(data[i, :].tolist())
+
+        for x_val, y_val, color in zip(x_vals, y_vals, colors):
+            ax1.scatter(x_val, y_val, s=5, c=color, alpha=0.5)
+
+        ax1.yaxis.grid()  # horizontal lines
+        ax1.set_xticklabels([method_name for method_name in method_names], fontsize=14)
+        for tick in ax1.yaxis.get_major_ticks():
+            tick.label.set_fontsize(14)
+        plt.setp(box['medians'], color='black')
+        plt.title(filenames[idx] + expressions[idx], fontsize=14)
+        plt.savefig(filenames[idx] + '.jpg', dpi=300)
+        plt.close()
+
 class Visualizer():
     def __init__(self, opt):
         self.opt = opt
